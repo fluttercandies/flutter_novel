2809f83563770b39217585371d2f86857679bffd
7-bit11 <210014468@qq.com>
2024-09-04 23:08:32 +0800

    ✨ feat(设置状态栏为透明):

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:novel_flutter_bit/frame.dart';
 
 void main() {
+  // 设置状态栏为透明
+  if (Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+  }
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
