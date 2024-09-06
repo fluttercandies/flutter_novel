87543aacaf519b7c1ca53ea272b6687ba602dc1e
7-bit11 <210014468@qq.com>
2024-09-06 15:42:08 +0800

    ✨ feat(NetStateTools 网络状态转换工具):

---
+++ lib/net/net_state.dart
@@ -0,0 +1,21 @@
+import 'package:novel_flutter_bit/base/base_state.dart';
+import 'package:novel_flutter_bit/net/service_result.dart';
+
+class NetStateTools {
+  /// successCode 即后端返回数据成功的code,这个参数是为了后端接口返回code不一致.
+  static NetState handle(ServiceResultData responseModel,
+      {int? successCode = 1}) {
+    if (responseModel.code == successCode) {
+      return NetState.dataSuccessState;
+    } else if (responseModel.code == 404) {
+      return NetState.error404State;
+    } else if (responseModel.code == -100) {
+      return NetState.timeOutState;
+    } else if (responseModel.code == 500) {
+      return NetState.errorShowRefresh;
+    } else if (responseModel.code == -1) {
+      return NetState.cancelRequest;
+    }
+    return NetState.unknown;
+  }
+}
