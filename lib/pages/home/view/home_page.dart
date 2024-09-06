d4c03b2dd1f39effb9be8d37384a6e0f796ac9ad
7-bit11 <210014468@qq.com>
2024-09-06 18:02:17 +0800

    ✨ feat(_buildReadItem):

--- lib/pages/home/view/home_page.dart
+++ lib/pages/home/view/home_page.dart
@@ -1,8 +1,10 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:novel_flutter_bit/base/base_provider.dart';
 import 'package:novel_flutter_bit/base/base_state.dart';
 import 'package:novel_flutter_bit/pages/home/view_model/view_model.dart';
 import 'package:novel_flutter_bit/tools/padding_extension.dart';
+import 'package:novel_flutter_bit/tools/size_extension.dart';
 import 'package:pull_to_refresh_notification/pull_to_refresh_notification.dart';
 
 class HomePage extends StatefulWidget {
@@ -18,7 +20,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    //_viewModel.getData();
+    _viewModel.getData();
   }
 
   @override
@@ -28,9 +30,9 @@ class _HomePageState extends State<HomePage> {
       body: ProviderConsumer<HomeViewModel>(
         viewModel: _viewModel,
         builder: (BuildContext context, HomeViewModel value, Widget? child) {
-          // if (value.homeState.netState == NetState.loadingState) {
-          //   return const Center(child: CircularProgressIndicator());
-          // }
+          if (value.homeState.netState == NetState.loadingState) {
+            return const Center(child: CircularProgressIndicator());
+          }
           return PullToRefreshNotification(
               onRefresh: value.onRefresh,
               child: CustomScrollView(
@@ -38,11 +40,103 @@ class _HomePageState extends State<HomePage> {
                   SliverPadding(
                       sliver: const SliverToBoxAdapter(
                           child: Text('我的阅读', style: TextStyle(fontSize: 20))),
-                      padding: 20.padding),
+                      padding: 10.padding),
+                  SliverToBoxAdapter(
+                    child: _buildReadList(value),
+                  )
                 ],
               ));
         },
       ),
+    );
+  }
+
+  /// 阅读列表
+  _buildReadList(HomeViewModel value) {
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: value.homeState.novelHot?.data?.length,
+          itemBuilder: (context, index) {
+            return _buildReadItem(
+                url: value.homeState.novelHot?.data?[index].img ?? "",
+                bookName: value.homeState.novelHot?.data?[index].name ?? "",
+                progress: .5);
+          }),
+    );
+  }
+
+  /// 阅读列表item
+  _buildReadItem(
+      {required String url,
+      required String bookName,
+      required double progress}) {
+    return Container(
+      margin: const EdgeInsets.all(5.0),
+      constraints: const BoxConstraints(maxWidth: 170, minWidth: 170),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8.0),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(.2),
+                blurRadius: 8.0,
+                spreadRadius: 0.5)
+          ]),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ExtendedImage.network(
+              url,
+              fit: BoxFit.cover,
+              cache: true,
+              loadStateChanged: (state) {
+                switch (state.extendedImageLoadState) {
+                  case LoadState.loading:
+                    return const CircularProgressIndicator();
+                  case LoadState.completed:
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(
+                            maxHeight: 240, minHeight: 240, minWidth: 160),
+                        child: ExtendedRawImage(
+                            image: state.extendedImageInfo?.image,
+                            fit: BoxFit.cover),
+                      ),
+                    );
+                  case LoadState.failed:
+                    return const Text("加载失败");
+                }
+              },
+            ),
+            Flexible(
+              child: Padding(
+                padding: 10.horizontal,
+                child: Text(bookName,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(fontSize: 15)),
+              ),
+            ),
+            Padding(
+              padding: 5.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Flexible(
+                    child: SizedBox(
+                        child: LinearProgressIndicator(value: progress)),
+                  ),
+                  5.horizontalSpace,
+                  Text('${progress * 100.toInt()}%')
+                ],
+              ),
+            )
+          ]),
     );
   }
 }
