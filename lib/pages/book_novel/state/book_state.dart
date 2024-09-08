49737db16379f838c95ed233e823ee344c63c2d0
7-bit11 <210014468@qq.com>
2024-09-08 15:11:05 +0800

    ✨ feat(BookState):

---
+++ lib/pages/book_novel/state/book_state.dart
@@ -0,0 +1,7 @@
+import 'package:novel_flutter_bit/base/base_state.dart';
+import 'package:novel_flutter_bit/pages/book_novel/entry/book_entry.dart';
+
+/// 小说来源详情页
+class BookState extends BaseState {
+  BookEntry? bookEntry;
+}
