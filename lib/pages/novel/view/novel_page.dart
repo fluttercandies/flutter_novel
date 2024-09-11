0483817e0b00946c213480cb6c6de8574b770b52
7-bit11 <210014468@qq.com>
2024-09-11 14:10:09 +0800

    ✨ feat(提交):

--- lib/pages/novel/view/novel_page.dart
+++ lib/pages/novel/view/novel_page.dart
@@ -4,6 +4,7 @@ import 'package:novel_flutter_bit/base/base_provider.dart';
 import 'package:novel_flutter_bit/base/base_state.dart';
 import 'package:novel_flutter_bit/pages/detail_novel/view/detail_page.dart';
 import 'package:novel_flutter_bit/pages/novel/view_model/novel_view_model.dart';
+import 'package:novel_flutter_bit/style/theme.dart';
 import 'package:novel_flutter_bit/tools/padding_extension.dart';
 import 'package:novel_flutter_bit/widget/empty.dart';
 import 'package:novel_flutter_bit/widget/loading.dart';
@@ -19,6 +20,7 @@ class NovelPage extends StatefulWidget {
 
 class _NovelPageState extends State<NovelPage> {
   late NovelViewModel _novelViewModel;
+
   @override
   void initState() {
     super.initState();
@@ -26,8 +28,36 @@ class _NovelPageState extends State<NovelPage> {
     _novelViewModel.getData();
   }
 
+  /// Convert HTML content to a list of InlineSpans
+  List<InlineSpan> _getTextSpan(
+      {required String htmlContent, required Color textColor}) {
+    List<InlineSpan> spans = <InlineSpan>[
+      // Convert HTML content to a list of InlineSpans with proper newlines
+    ];
+
+    // Replace &nbsp; with a regular space
+    String plainText = htmlContent.replaceAll(RegExp(r'&nbsp;'), ' ');
+    String plainText1 = plainText.replaceAll(RegExp(r'</p>'), ' ');
+    // Split the text into lines using the newline character
+    List<String> lines = plainText1.split('<br />');
+
+    for (String line in lines) {
+      if (line.isNotEmpty) {
+        spans.add(TextSpan(
+            // ignore: prefer_const_constructors
+            text: line,
+            style: TextStyle(fontSize: 16, color: textColor)));
+        // Add a newline (invisible container with zero height)
+        spans.add(WidgetSpan(child: Container(height: 0)));
+      }
+    }
+    return spans;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final MyColorsTheme myColors =
+        Theme.of(context).extension<MyColorsTheme>()!;
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.name),
@@ -42,16 +72,21 @@ class _NovelPageState extends State<NovelPage> {
           if (value.novelState.netState == NetState.emptyDataState) {
             return const EmptyBuild();
           }
-          return Padding(
-            padding: 10.padding,
-            child: SingleChildScrollView(
-              child: Text(
-                value.novelState.novelEntry.data?.text ?? '',
-              ),
-            ),
-          );
+          return _buildSuccess(value, myColors: myColors);
         },
       ),
+    );
+  }
+
+  /// 构建成功
+  _buildSuccess(NovelViewModel value, {required MyColorsTheme myColors}) {
+    return SingleChildScrollView(
+      padding: 20.padding,
+      child: RichText(
+          text: TextSpan(
+              children: _getTextSpan(
+                  textColor: myColors.textColorHomePage!,
+                  htmlContent: value.novelState.novelEntry.data?.text ?? ''))),
     );
   }
 }
