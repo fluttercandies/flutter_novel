ea5934686c407dc7034d7dd434fc96a0a78c3d96
7-bit11 <210014468@qq.com>
2024-09-17 17:24:30 +0800

    ✨ feat(设置字体大小):

--- lib/widget/show_slider_sheet.dart
+++ lib/widget/show_slider_sheet.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
+import 'package:novel_flutter_bit/pages/book_novel/view/book_page.dart';
+import 'package:novel_flutter_bit/pages/novel/view/novel_page.dart';
+import 'package:novel_flutter_bit/tools/size_extension.dart';
 import 'package:novel_flutter_bit/widget/slider_novel.dart';
 
 class ShowSliderSheet extends StatefulWidget {
@@ -20,26 +23,44 @@ class ShowSliderSheet extends StatefulWidget {
 class _ShowSliderSheetState extends State<ShowSliderSheet> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 500,
-      width: double.infinity,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            const Text("设置", style: TextStyle(fontSize: 20)),
-            SliderNovel(
-              color: widget.color,
-              value: widget.value,
-              onChanged: (p0) {
-                widget.value = p0;
-                setState(() {});
-                if (widget.onChanged != null) {
-                  widget.onChanged!(widget.value);
-                }
-              },
-            ),
-          ],
+    ThemeData theme = Theme.of(context);
+    return DefaultTextStyle(
+      style: TextStyle(
+          fontWeight: FontWeight.w300,
+          color: theme.textTheme.bodyLarge?.color,
+          fontSize: 18),
+      child: SizedBox(
+        height: 500,
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              10.verticalSpace,
+              const Align(
+                  child: Text(
+                "设置",
+                style: TextStyle(fontSize: 20),
+              )),
+              10.verticalSpace,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Text("字体大小：${NovelSize.size.toInt()}"),
+              ),
+              SliderNovel(
+                color: widget.color,
+                value: widget.value,
+                onChanged: (p0) {
+                  widget.value = p0;
+                  setState(() {});
+                  if (widget.onChanged != null) {
+                    widget.onChanged!(widget.value);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
