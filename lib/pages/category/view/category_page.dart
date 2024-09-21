4527a032a172d6c2ec5fda9fb2040e9b8f22c858
7-bit11 <210014468@qq.com>
2024-09-21 22:26:44 +0800

    ✨ feat(CustomScrollView):

--- lib/pages/category/view/category_page.dart
+++ lib/pages/category/view/category_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:novel_flutter_bit/pages/category/enum/category_enum.dart';
 import 'package:novel_flutter_bit/pages/category/view_model/category_view_model.dart';
 import 'package:novel_flutter_bit/tools/padding_extension.dart';
+import 'package:novel_flutter_bit/tools/size_extension.dart';
 import 'package:novel_flutter_bit/widget/empty.dart';
 import 'package:novel_flutter_bit/widget/loading.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -25,17 +26,24 @@ class _CategoryPageState extends ConsumerState<CategoryPage> {
 
   @override
   Widget build(BuildContext context) {
-    // final categoryViewModel = ref.watch(
-    //     categoryViewModelProvider(categoryEnum: _categoryList[_currentIndex]));
+    final categoryViewModel = ref.watch(
+        categoryViewModelProvider(categoryEnum: _categoryList[_currentIndex]));
     _theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(title: const Text('分类列表')),
-      body: Padding(
-        padding: 20.padding,
-        child: Column(children: [
-          _buildCateGoryList(),
-        ]),
-      ),
+      body: CustomScrollView(slivers: [
+        _buildCateGoryList(),
+        SliverPadding(
+            padding: 10.padding,
+            sliver: const SliverToBoxAdapter(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('全网最热-分类', style: TextStyle(fontSize: 16)),
+                Icon(Icons.chevron_right)
+              ],
+            ))),
+      ]),
       // switch (categoryViewModel) {
       //   AsyncData(:final value) => Builder(builder: (BuildContext context) {
       //       //LoggerTools.looger.e(value.netState);
@@ -49,16 +57,29 @@ class _CategoryPageState extends ConsumerState<CategoryPage> {
   }
 
   /// 构建分类列表
-  _buildCateGoryList() {
-    return MasonryGridView.count(
-      shrinkWrap: true,
-      itemCount: _categoryList.length,
-      crossAxisCount: 3,
-      mainAxisSpacing: 10,
-      crossAxisSpacing: 8,
-      itemBuilder: (context, index) {
-        return _buildCateGoryItem(_categoryList[index]);
-      },
+  SliverPadding _buildCateGoryList() {
+    return SliverPadding(
+      padding: 10.padding,
+      sliver: SliverToBoxAdapter(
+        child: MasonryGridView.count(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          itemCount: _categoryList.length,
+          crossAxisCount: 3,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 8,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              child: _buildCateGoryItem(_categoryList[index]),
+            );
+          },
+        ),
+      ),
     );
   }
 
@@ -70,12 +91,13 @@ class _CategoryPageState extends ConsumerState<CategoryPage> {
       color = _theme.primaryColor;
       textColor = Colors.white;
     }
-    return Container(
+    return AnimatedContainer(
       alignment: Alignment.center,
       padding: const EdgeInsets.symmetric(vertical: 8),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(5),
           color: color), //_theme.primaryColor.withOpacity()
+      duration: Durations.short2,
       child: Text(categoryEnum.name, style: TextStyle(color: textColor)),
     );
   }
