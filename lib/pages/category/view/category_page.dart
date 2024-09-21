d2c048db845a91f1753f7a5d3b1d82cf3064f211
7-bit11 <210014468@qq.com>
2024-09-21 21:19:36 +0800

    ✨ feat(CategoryPage):

---
+++ lib/pages/category/view/category_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CategoryPage extends ConsumerStatefulWidget {
+  const CategoryPage({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends ConsumerState<CategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
