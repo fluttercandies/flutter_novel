1eb252974efabc12b3ef9a201b5140b0f0558bfc
7-bit11 <210014468@qq.com>
2024-09-21 22:38:11 +0800

    ✨ feat(分类页面成功):

--- lib/pages/category/view/category_page.dart
+++ lib/pages/category/view/category_page.dart
@@ -1,10 +1,17 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:novel_flutter_bit/pages/category/enum/category_enum.dart';
+import 'package:novel_flutter_bit/pages/category/state/category_state.dart';
 import 'package:novel_flutter_bit/pages/category/view_model/category_view_model.dart';
+import 'package:novel_flutter_bit/pages/home/entry/novel_hot_entry.dart';
+import 'package:novel_flutter_bit/route/route.gr.dart';
+import 'package:novel_flutter_bit/tools/net_state_tools.dart';
 import 'package:novel_flutter_bit/tools/padding_extension.dart';
 import 'package:novel_flutter_bit/tools/size_extension.dart';
 import 'package:novel_flutter_bit/widget/empty.dart';
+import 'package:novel_flutter_bit/widget/image.dart';
 import 'package:novel_flutter_bit/widget/loading.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
@@ -24,6 +31,11 @@ class _CategoryPageState extends ConsumerState<CategoryPage> {
 
   late ThemeData _theme;
 
+  /// 跳转小说 站源 列表 页面
+  _onToBookPage(String name) {
+    context.router.push(BookRoute(name: name));
+  }
+
   @override
   Widget build(BuildContext context) {
     final categoryViewModel = ref.watch(
@@ -31,29 +43,56 @@ class _CategoryPageState extends ConsumerState<CategoryPage> {
     _theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(title: const Text('分类列表')),
-      body: CustomScrollView(slivers: [
-        _buildCateGoryList(),
-        SliverPadding(
-            padding: 10.padding,
-            sliver: const SliverToBoxAdapter(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('全网最热-分类', style: TextStyle(fontSize: 16)),
-                Icon(Icons.chevron_right)
-              ],
-            ))),
-      ]),
-      // switch (categoryViewModel) {
-      //   AsyncData(:final value) => Builder(builder: (BuildContext context) {
-      //       //LoggerTools.looger.e(value.netState);
-      //       return NetStateTools.getWidget(value.netState) ??
-      //           _buildSuccess(value: value);
-      //     }),
-      //   AsyncError() => const EmptyBuild(),
-      //   _ => const LoadingBuild(),
-      // }
+      body: SafeArea(
+        child: DefaultTextStyle(
+          style: Theme.of(context)
+              .textTheme
+              .bodyLarge!
+              .copyWith(fontSize: 17, fontWeight: FontWeight.w300),
+          child: CustomScrollView(slivers: [
+            _buildCateGoryList(),
+            _buildTitle(),
+            switch (categoryViewModel) {
+              AsyncData(:final value) =>
+                Builder(builder: (BuildContext context) {
+                  //LoggerTools.looger.e(value.netState);
+                  final data = NetStateTools.getWidget(value.netState);
+                  if (data != null) {
+                    return SliverToBoxAdapter(child: data);
+                  }
+                  return _buildSuccess(value: value);
+                }),
+              AsyncError() => const EmptyBuild(),
+              _ => const LoadingBuild(),
+            }
+          ]),
+        ),
+      ),
     );
+  }
+
+  /// 构建成功
+  SliverList _buildSuccess({required CategoryState value}) {
+    return SliverList.builder(
+        itemCount: value.novelHot?.data?.length,
+        itemBuilder: (context, index) {
+          return _buildHotItem(value.novelHot?.data?[index],
+              onTap: _onToBookPage);
+        });
+  }
+
+  /// 构建标题
+  SliverPadding _buildTitle() {
+    return SliverPadding(
+        padding: 10.padding,
+        sliver: SliverToBoxAdapter(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('全网最热-分类', style: TextStyle(color: _theme.primaryColor)),
+            const Icon(Icons.chevron_right)
+          ],
+        )));
   }
 
   /// 构建分类列表
@@ -98,7 +137,68 @@ class _CategoryPageState extends ConsumerState<CategoryPage> {
           borderRadius: BorderRadius.circular(5),
           color: color), //_theme.primaryColor.withOpacity()
       duration: Durations.short2,
-      child: Text(categoryEnum.name, style: TextStyle(color: textColor)),
+      child: Text(categoryEnum.name,
+          style: TextStyle(
+              color: textColor, fontSize: 14, fontWeight: FontWeight.w400)),
+    );
+  }
+
+  /// 热门item
+  _buildHotItem(Datum? novelHot,
+      {double height = 180, required void Function(String str) onTap}) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {
+        onTap.call(novelHot?.name ?? "");
+      },
+      child: SizedBox(
+        height: height,
+        child: Padding(
+          padding: 10.padding,
+          child: Row(
+            children: [
+              Flexible(
+                  child: ExtendedImageBuild(
+                      url: novelHot?.img ?? "", width: 120, height: height)),
+              10.horizontalSpace,
+              Expanded(
+                flex: 2,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(novelHot?.name ?? "",
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontSize: 20)),
+                      5.verticalSpace,
+                      Text("${novelHot?.author}/${novelHot?.type}",
+                          style: const TextStyle(color: Colors.black54)),
+                      5.verticalSpace,
+                      Row(children: [
+                        SvgPicture.asset(
+                          'assets/svg/hot.svg',
+                          width: 24,
+                        ),
+                        5.horizontalSpace,
+                        Text(novelHot?.hot ?? "0",
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor)),
+                      ]),
+                      5.verticalSpace,
+                      Flexible(
+                        child: Text(novelHot?.desc ?? "",
+                            maxLines: 4,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.black54)),
+                      )
+                    ]),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
