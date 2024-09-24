9113b0caae4dbe7a51a48947f4d0bc806dcf058a
7-bit11 <210014468@qq.com>
2024-09-24 15:42:39 +0800

    ✨ feat(CollectState):

---
+++ lib/pages/collect_novle/state/collect_state.dart
@@ -0,0 +1,6 @@
+import 'package:novel_flutter_bit/base/base_state.dart';
+import 'package:novel_flutter_bit/pages/collect_novle/enrty/collect_entry.dart';
+
+class CollectState extends BaseState {
+  List<CollectNovelEntry>? collectNovelList;
+}
