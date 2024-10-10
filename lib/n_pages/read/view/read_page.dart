ef2635c5864282ecf2ab8c6b9629fff99ac73345
7-bit11 <210014468@qq.com>
2024-10-10 17:59:07 +0800

    ✨ feat(页面解析，多级页面):

--- lib/n_pages/read/view/read_page.dart
+++ lib/n_pages/read/view/read_page.dart
@@ -1,9 +1,17 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:extended_text/extended_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:novel_flutter_bit/entry/book_source_entry.dart';
 import 'package:novel_flutter_bit/n_pages/detail/entry/detail_book_entry.dart';
+import 'package:novel_flutter_bit/n_pages/read/state/read_state.dart';
 import 'package:novel_flutter_bit/n_pages/read/view_model/read_view_model.dart';
+import 'package:novel_flutter_bit/pages/novel/state/novel_read_state.dart';
+import 'package:novel_flutter_bit/tools/padding_extension.dart';
+import 'package:novel_flutter_bit/widget/empty.dart';
+import 'package:novel_flutter_bit/widget/loading.dart';
+import 'package:novel_flutter_bit/widget/net_state_tools.dart';
+import 'package:novel_flutter_bit/widget/special_text_span_builder.dart';
 
 @RoutePage()
 class ReadPage extends ConsumerStatefulWidget {
@@ -15,14 +23,72 @@ class ReadPage extends ConsumerStatefulWidget {
 }
 
 class _ReadPageState extends ConsumerState<ReadPage> {
+  ThemeData get _themeData => Theme.of(context);
+  final NovelSpecialTextSpanBuilder _specialTextSpanBuilder =
+      NovelSpecialTextSpanBuilder(color: Colors.black);
+  late TextStyle _style;
+
+  /// 动画时长
+  final Duration _duration = const Duration(milliseconds: 400);
+
+  /// 控制AppBar和BottomNavigationBar的可见性
+  bool _isAppBarVisible = false;
+  bool _isBottomBarVisible = false;
+
+  /// 构建初始数据
+  void _buildInitData() {
+    _specialTextSpanBuilder.color = _themeData.primaryColor;
+    _style = TextStyle(
+        fontSize: NovelReadState.size,
+        fontWeight: NovelReadState.weight.fontWeight,
+        color: _themeData.textTheme.bodyLarge?.color);
+    _specialTextSpanBuilder.color =
+        _themeData.textTheme.bodyMedium?.color ?? Colors.black;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _buildInitData();
     final readViewModel = ref.watch(readViewModelProvider(
         chapter1: widget.chapter, bookSource: widget.source));
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.chapter.chapterName ?? " 暂无标题"),
         ),
-        body: Text((readViewModel.value?.content ?? "").trim()));
+        body: switch (readViewModel) {
+          AsyncData(:final value) => Builder(builder: (BuildContext context) {
+              Widget? child = NetStateTools.getWidget(value.netState);
+              if (child != null) {
+                return child;
+              }
+              return _buildSuccess(value: value, style: _style);
+            }),
+          AsyncError() => EmptyBuild(),
+          _ => const LoadingBuild(),
+        });
+  }
+
+  /// 构建成功
+  _buildSuccess({required ReadState value, required TextStyle style}) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () {},
+      child: Center(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: SingleChildScrollView(
+              padding: 20.padding,
+              child: ExtendedText.rich(TextSpan(children: [
+                _specialTextSpanBuilder.build(value.content ?? '',
+                    textStyle: style)
+              ]))),
+        ),
+      ),
+    );
   }
 }
--- lib/n_pages/read/view_model/read_view_model.dart
+++ lib/n_pages/read/view_model/read_view_model.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: control_flow_in_finally
+
 import 'dart:convert';
 
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
@@ -25,6 +27,7 @@ class ReadViewModel extends _$ReadViewModel {
   late Chapter chapter;
 
   late BookSourceEntry _bookSourceEntry;
+
   @override
   Future<ReadState> build({
     required Chapter chapter1,
@@ -41,27 +44,116 @@ class ReadViewModel extends _$ReadViewModel {
     required String detailUrl,
   }) async {
     try {
+      final data = await _initDataAll(detailUrl: detailUrl, str: "");
+      if (data == "") {
+        readState.netState = NetState.emptyDataState;
+        state = AsyncData(readState);
+        return;
+      }
+      readState.netState = NetState.dataSuccessState;
+      readState.content = data;
+      state = AsyncData(readState);
+      // LoggerTools.looger.d(data);
+    } catch (e) {
+      LoggerTools.looger.e("NewSearchViewModel _initData error:$e");
+      // searchState.netState = NetState.error403State;
+      // state = AsyncData(searchState);
+      SmartDialog.showToast(e.toString());
+    }
+  }
+
+  Future<String> _initDataAll({
+    required String detailUrl,
+    required String str,
+  }) async {
+    try {
       final resultData = await NewNovelHttp().request(detailUrl);
       final uint8List = resultData.data;
       resultData.data = ParseSourceRule.parseHtmlDecode(uint8List);
       final detailBook = ParseSourceRule.parseAllMatches(
           rule: _bookSourceEntry.ruleContent?.content ?? "",
           htmlData: resultData.data);
+      final detailNextBook = ParseSourceRule.parseAllMatches(
+          rule: _bookSourceEntry.ruleContent?.nextContentUrl ?? "",
+          htmlData: resultData.data);
 
-      if (detailBook.isEmpty) {
-        readState.netState = NetState.emptyDataState;
-        state = AsyncData(readState);
-        return;
+      final url =
+          _getChapterList(_bookSourceEntry.bookSourceUrl ?? "", detailNextBook);
+
+      str += detailBook[0] ?? "";
+      LoggerTools.looger.d(str);
+      if (url.isEmpty) {
+        return str;
       }
-      readState.content = detailBook[0];
-      readState.netState = NetState.dataSuccessState;
-      state = AsyncData(readState);
-      LoggerTools.looger.d(detailBook.toString());
+      if (detailNextBook.isEmpty || detailNextBook.length > 2) {
+        return str;
+      }
+      return await _initDataAll(detailUrl: url[0] ?? "", str: str);
     } catch (e) {
       LoggerTools.looger.e("NewSearchViewModel _initData error:$e");
       // searchState.netState = NetState.error403State;
       // state = AsyncData(searchState);
       SmartDialog.showToast(e.toString());
+      return "error";
+    }
+  }
+
+  List<String?> _getChapterList(String url, List<String?> list) {
+    List<String> resultList = [];
+    for (var i = 0; i < list.length; i++) {
+      if (list[i]!.startsWith("https://") || list[i]!.startsWith("http://")) {
+        return list;
+      } else {
+        if (!list[i]!.startsWith("/")) {
+          // 去除baseUrl末尾的斜杠（如果有）
+          if (url.endsWith('/')) {
+            url = url.substring(0, url.length - 1);
+          }
+          // 如果relativeUrl以斜杠开头，则去除
+          if (list[i]!.startsWith('/')) {
+            list[i] = list[i]!.substring(1);
+          }
+
+          if (list[i]!.contains("javascript:Next")) {
+            final map = extractChapterInfo(list[i]!);
+            final url1 = "${map['chapterId']}/${map['htmlFileName']}";
+            if (list.length > i) {
+              url = "$url/read/$url1";
+              resultList.add(url);
+              continue;
+            }
+          }
+          resultList.add("$url/${list[i] ?? ""}");
+        } else {
+          // 去除baseUrl末尾的斜杠（如果有）
+          if (url.endsWith('/')) {
+            url = url.substring(0, url.length - 1);
+          }
+          // 如果relativeUrl以斜杠开头，则去除
+          if (list[i]!.startsWith('/')) {
+            list[i] = list[i]!.substring(1);
+          }
+
+          resultList.add("$url/${list[i] ?? ""}");
+        }
+      }
+    }
+    return resultList;
+  }
+
+  Map<String, String> extractChapterInfo(String input) {
+    // 定义正则表达式
+    var regex =
+        RegExp("Next\\(\\'(\\d+)\\',\\'(.+?)\\'\\)", caseSensitive: false);
+    // 使用正则表达式匹配输入字符串
+    final Match? match = regex.firstMatch(input);
+
+    if (match != null && match.groupCount == 2) {
+      // 如果匹配成功，提取章节编号和HTML文件名
+      return {'chapterId': match.group(1)!, 'htmlFileName': match.group(2)!};
+    } else {
+      // 如果没有匹配到，返回空的Map
+      return {};
     }
   }
 }
