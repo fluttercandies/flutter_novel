11e0dac855488772f1671e77b791e1c00a108732
7-bit11 <210014468@qq.com>
2024-10-21 11:46:59 +0800

    ✨ feat(历史记录页面view):

--- lib/n_pages/history/view/history_page.dart
+++ lib/n_pages/history/view/history_page.dart
@@ -1,6 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:novel_flutter_bit/n_pages/history/entry/history_entry.dart';
+import 'package:novel_flutter_bit/n_pages/history/state/history_state.dart';
+import 'package:novel_flutter_bit/n_pages/history/view_model/history_view_model.dart';
+import 'package:novel_flutter_bit/widget/empty.dart';
+import 'package:novel_flutter_bit/widget/loading.dart';
+import 'package:novel_flutter_bit/widget/net_state_tools.dart';
 
 @RoutePage()
 class HistoryPage extends ConsumerStatefulWidget {
@@ -13,10 +19,36 @@ class HistoryPage extends ConsumerStatefulWidget {
 class _HistoryPageState extends ConsumerState<HistoryPage> {
   @override
   Widget build(BuildContext context) {
+    final historyViewModel = ref.watch(historyViewModelProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text("阅读记录"),
       ),
+      body: switch (historyViewModel) {
+        AsyncData(:final value) => Builder(builder: (_) {
+            Widget? child =
+                NetStateTools.getWidget(value.netState, msg: "还没有历史记录哦！");
+            if (child != null) {
+              return child;
+            }
+            return _buildSuccess(value);
+          }),
+        AsyncError() => EmptyBuild(),
+        _ => const LoadingBuild(),
+      },
     );
+  }
+
+  Widget _buildSuccess(HistoryState value) {
+    return ListView.builder(
+      itemBuilder: (c, i) {
+        return _buildItem(value.historyList![i]);
+      },
+      itemCount: value.historyList?.length,
+    );
+  }
+
+  _buildItem(HistoryEntry entry) {
+    return Text(entry.chapter?.chapterName ?? "");
   }
 }
