663ff986220932da8df83bb0b860ff82cdeec6d2
7-bit11 <210014468@qq.com>
2024-10-21 11:31:13 +0800

    ✨ feat(HistoryState 历史记录状态):

---
+++ lib/n_pages/history/state/history_state.dart
@@ -0,0 +1,6 @@
+import 'package:novel_flutter_bit/base/base_state.dart';
+import 'package:novel_flutter_bit/n_pages/history/entry/history_entry.dart';
+
+class HistoryState extends BaseState {
+  List<HistoryEntry>? likeList;
+}
